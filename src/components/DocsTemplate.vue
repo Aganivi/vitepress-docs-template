4592358ce8b01c896b9f646dde04569084f578ec
Agan <1571698188@qq.com>
2024-08-08 16:13:31 +0800

    update docsTemplate

--- src/components/DocsTemplate.vue
+++ src/components/DocsTemplate.vue
@@ -2,17 +2,14 @@
 div(v-html="md")
 </template>
 <script setup>
-import { data } from '../template.data.js'
 // data
 const route = useRoute()
+const { theme } = useData()
 const fileKey = `docs/${route.data.filePath}`
+const templateMap = theme.value.docsTemplate
 
-const md = computed(() => data[fileKey])
+const md = computed(() => templateMap[fileKey])
 // methods
 // lifecycle
 </script>
-<style lang="stylus" rel="stylesheet/stylus" scoped>
-. {
-
-}
-</style>
+<style lang="stylus" rel="stylesheet/stylus" scoped></style>
